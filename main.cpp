00b9479049580c522f5aef4166d2a334fcf83ac1
oeeea51-sketch <oeeea51@gmail.com>
2026-06-25 00:21:12 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/SetupMovePopup.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MySetupMovePopup, SetupMovePopup) {
+public:
+    bool init(EffectGameObject* trigger, cocos2d::CCArray* objects) {
+        if (!SetupMovePopup::init(trigger, objects))
+            return false;
+
+        // Создаем красивую зеленую кнопку "Confirm"
+        auto confirmSpr = ButtonSprite::create(
+            "Confirm",
+            60,
+            true,
+            "goldFont.fnt",
+            "GJ_button_02.png", 
+            25.0f,
+            0.5f
+        );
+
+        auto confirmBtn = CCMenuItemSpriteExtra::create(
+            confirmSpr,
+            this,
+            menu_selector(MySetupMovePopup::onConfirmID)
+        );
+
+        // Позиция кнопки снизу по центру меню
+        confirmBtn->setPosition({0.f, -115.f});
+
+        // Безопасно добавляем кнопку в меню триггера
+        if (auto menu = this->m_mainLayer->getChildByID("main-menu")) {
+            menu->addChild(confirmBtn);
+        } else if (auto menu = this->m_buttonMenu) {
+            menu->addChild(confirmBtn);
+        }
+
+        return true;
+    }
+
+    void onConfirmID(cocos2d::CCObject*) {
+        auto editorLayer = LevelEditorLayer::get();
+        if (!editorLayer) return;
+
+        auto objects = editorLayer->m_objects;
+        if (!objects || objects->count() < 2) return;
+
+        // Ищем объект, который был добавлен или изменен прямо перед триггером
+        GameObject* targetObj = nullptr;
+        for (int i = objects->count() - 1; i >= 0; --i) {
+            auto obj = static_cast<GameObject*>(objects->objectAtIndex(i));
+            if (obj && obj != m_gameObject && obj->m_groupCount > 0) {
+                targetObj = obj;
+                break;
+            }
+        }
+
+        if (!targetObj) {
+            FLAlertLayer::create("Error", "No object with Group ID found!", "OK")->show();
+            return;
+        }
+
+        // Берем самую первую группу объекта
+        int groupID = targetObj->m_groups->at(0);
+        if (groupID <= 0) return;
+
+        // Записываем ID в текстовое поле Move-триггера
+        if (this->m_targetGroupInput) {
+            this->m_targetGroupInput->setString(std::to_string(groupID).c_str());
+        }
+
+        // Обновляем значение в самом триггере, чтобы игра его запомнила
+        if (this->m_gameObject) {
+            this->m_gameObject->m_targetGroupID = groupID;
+        }
+    }
+};
